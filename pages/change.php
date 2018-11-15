dca42b051a0ec52a0f66ccda1ee95b0f28a9c85c
Winzmcman <nik.pawlo2011@yandex.ua>
2018-11-15 17:09:49 +0200

    1.2 vk/skype (fixed)

--- pages/change.php
+++ pages/change.php
@@ -12,24 +12,25 @@ Controller::action('change');
         <title></title>
 </head>
 <body>
-    <form action="" method="post" onsubmit="return valform()">
+    <form action="" method="post" >
         <a href="<?= Controller::url('profile') ?>">Вернуться в аккаунт</a>
         <br/>
         <label for="">Новый пароль<br/><input type="text" name="newpass"><br/><br/></label>
         <label for="">Новый vk<br/><input type="text" name="vk"><br/><br/></label>
         <label for="">Новый skype<br/><input type="text" name="skype"><br/><br/></label>
-        <input type="submit" value="Изменить пароль">
+        <input type="submit" value="Изменить пароль" onclick="return valform()">
     </form>
 <script>
     function valform(){
-        var pass document.getElementsByName("newpass")[0].value;
-        var vk document.getElementsByName("vk")[0].value;
-        var skype document.getElementsByName("skype")[0].value;
+        var pass = document.getElementsByName('newpass')[0].value;
+        var vk = document.getElementsByName('vk')[0].value;
+        var skype = document.getElementsByName('skype')[0].value;
         if(pass == ""){
 
         }
         else{
-            if(pass.value.length < 8){
+            if(pass.length < 8){
+                alert('Пароль не должен быть менее 8 символов');
                 return false;
             }
         }
--- pages/forget.php
+++ pages/forget.php
@@ -16,7 +16,18 @@ Controller::action('forget');
         <label for="">Введите ваш login<br/><input type="text" name="login"><br/></label>
         <label for="">Введите секретное слово, указанное при регистрации<br/><input type="text" name="secret"><br/></label>
         <label for="">Введите новый пароль<br/><input type="text" name="newpass"><br/></label>
-        <input type="submit" value="Восстановить аккаунт">
+        <input type="submit" value="Восстановить аккаунт" onclick="return valform()">
     </form>
+<script>
+    function valform(){
+        var pass = document.getElementsByName('newpass')[0].value;
+        if(pass.length < 8){
+            alert('Пароль не должен быть менее 8 символов');
+            return false;
+        }
+
+        return true;
+    }
+</script>
 </body>
 </html>
