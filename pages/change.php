1d9aca968e15e5fb810e66c9dd451990276bb640
Winzmcman <nik.pawlo2011@yandex.ua>
2018-11-15 16:52:41 +0200

    1.2 skype & vk

--- pages/change.php
+++ pages/change.php
@@ -12,11 +12,30 @@ Controller::action('change');
         <title></title>
 </head>
 <body>
-    <form action="" method="post">
+    <form action="" method="post" onsubmit="return valform()">
         <a href="<?= Controller::url('profile') ?>">Вернуться в аккаунт</a>
         <br/>
         <label for="">Новый пароль<br/><input type="text" name="newpass"><br/><br/></label>
+        <label for="">Новый vk<br/><input type="text" name="vk"><br/><br/></label>
+        <label for="">Новый skype<br/><input type="text" name="skype"><br/><br/></label>
         <input type="submit" value="Изменить пароль">
     </form>
+<script>
+    function valform(){
+        var pass document.getElementsByName("newpass")[0].value;
+        var vk document.getElementsByName("vk")[0].value;
+        var skype document.getElementsByName("skype")[0].value;
+        if(pass == ""){
+
+        }
+        else{
+            if(pass.value.length < 8){
+                return false;
+            }
+        }
+
+        return true;
+    }
+</script>
 </body>
 </html>
